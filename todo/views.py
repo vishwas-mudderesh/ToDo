b7d61af08dd5a2cd43273633cdadeeaa961dfb68
vishwas-mudderesh <vishwas0000.m@gmail.com>
2025-09-05 13:11:10 +0530

    second commit

---
+++ todo/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from .models import Task
+
+# Create your views here.
+def addTask(request):
+    task = request.POST['task']
+    Task.objects.create(task=task)
+    return redirect('home')
